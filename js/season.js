59fa1868bd05e724b50d4996b4829e8b23fa23ac
EdgarLee0816 <ryunoske84@gmail.com>
2024-08-22 11:19:20 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 //링크가 이동된 후 해당 페이지로 ?p0 인수를 전달한다.
-  const para = new URLSearchParams(window.location.search); // (쿼리 스트링 부분)
+  const para = new URLSearchParams(window.location.search); // (쿼리 스트링 부분, ?가 퀴리 부분 p0가 스트링 부분)
   
     //페이지가 로드 된 후 실행
     const menu = document.getElementById("menu");// <iframe id ="menu">
