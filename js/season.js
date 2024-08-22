e3472f8123ae5c54caf40c82b8199cc857c55750
EdgarLee0816 <ryunoske84@gmail.com>
2024-08-22 11:31:23 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
     //페이지가 로드 된 후 실행
     const menu = document.getElementById("menu");// <iframe id ="menu">
     
-    menu.addEventListener("load", () => {
+    /* menu.addEventListener("load", () => {
         // 부모페이지에서 자식 아이프레임의 콘텐츠에 접근 가능해야 한다.
         const nav = menu.contentDocument.querySelectorAll("nav a"); // 각각각 메뉴 <a href="spring">
         /* nav.forEach( i => {
             i.classList.add("act");
         }); */
-    });
+    }); */
 
 
 
