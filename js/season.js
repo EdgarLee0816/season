0ead2326eb9df7ddbf594561ce53c2312364cb53
EdgarLee0816 <ryunoske84@gmail.com>
2024-08-22 10:48:57 +0900

    Add files via upload

---
+++ js/season.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menu = document.getElementById("menu");// <iframe id ="menu">
+    const nav = menu.contentDocument.querySelectorAll("nav a"); // 각각각 메뉴 <a href="spring">
+
+    //페이지가 로드 된 후 실행
+    menu.addEventListener("load", () => {
+        // 부모페이지에서 자식 아이프레임의 콘텐츠에 접근 가능해야 한다.
+
+        nav.forEach( i => {
+            i.classList.add("act");
+        });
+    });
+
+
+
+});/////////////////////////끝
